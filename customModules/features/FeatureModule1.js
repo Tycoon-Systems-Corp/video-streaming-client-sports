971c696ecbb51423a47f952ab2b27c18219d2a9e
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2024-05-08 12:40:53 -0400

    map through articles when rendering

--- customModules/features/FeatureModule1.js
+++ customModules/features/FeatureModule1.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { fetchArticle } from "./articleRequest"
 
 const Module = props => {
+    const [ componentDidMount, setComponentDidMount ] = React.useState(false)
     const [ fetchBusy, setFetchBusy ] = React.useState(false)
     const [ pageError, setPageError ] = React.useState(null)
     const [ validCc, setValidCc ] = React.useState(true)
@@ -9,16 +10,20 @@ const Module = props => {
     const [ articleData, setArticleData ] = React.useState(null)
 
     React.useEffect(() => {
-        fetchArticle().then((data) => {
-        articleData ? setArticleData(data) : null
-        console.log("DATA FROM COMPONENT:", articleData.status)
-    })
+        if (!componentDidMount) {
+            setComponentDidMount(true)
+            fetchArticle().then((response) => {
+                console.log(response)
+                response ? setArticleData(response.data.fetchedData[0].articleReq[0]) : "test"
+            })
+        } 
     },[])
     // console.log(fetchArticle())
 
     return (
         <React.Fragment>
-            <div>{articleData ? articleData.status : "loading..."}</div>
+            {console.log("ARTICLE STATE:", articleData)}
+            {articleData ? articleData.map(article => <div>{article.title}</div>) : "loading..."}
         </React.Fragment>
     )
 }
